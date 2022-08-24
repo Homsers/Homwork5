6406b39f88a5cb2925dc6d3ab791bbdf28e60507
Maxim <you@example.com>
2022-08-24 14:10:11 +0300

    1com

---
+++ ts1/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Enter a number of the array");
+int numbers = int.Parse(Console.ReadLine());
+
+int[] GetArray(int size, int minValue,int maxValue)
+{   
+    int[] res = new int[size];
+    Console.Write("Array: ");
+
+    for (int i=0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return res;
+}
+
+int[] array = GetArray(numbers, 100, 999);
+
+
+int even = 0;
+int odd = 0;
+foreach(int g in array)
+{
+    if 
+    (g % 2 == 0)
+    even++;
+    
+    else 
+    odd++;
+}
+
+Console.WriteLine(String.Join(" ", array));
+Console.WriteLine($"{even} numbers is even");
+Console.WriteLine($"{odd} numbers is odd");
---
+++ ts2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Enter a number of the array");
+int numbers = int.Parse(Console.ReadLine());
+
+int GetArray(int size, int minValue, int maxValue)
+  {
+    int[] res = new int[size];
+    int sum = 0;
+    Console.Write("Array:");
+
+
+    for (int i=0; i<res.Length;i++)
+    {
+    res[i] = new Random().Next(minValue, maxValue+1);
+
+    Console.Write(res[i] + " ");
+
+    if( i % 2 != 0)
+    {
+        sum = sum + res[i];
+    }
+   }
+  return sum;
+  }
+  int res = GetArray(numbers, -10, 10);
+
+  Console.WriteLine($"\nSum of the odd places {res} ");
